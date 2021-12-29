fc833698dc00b85c03094f1b6bf69449b5c99a44
nuryhan <nurmuhammet.muradov0309@gmail.com>
2021-12-29 12:48:15 +0500

    todo app

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,69 @@
+import React, {Fragment, useEffect, useState } from 'react'
+
+const ListTodos = () => {
+
+    const [todos,setTodos] = useState([]);
+
+    // delete todo
+    const deleteTodo = async (id) =>{
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            })
+            setTodos(todos.filter(todo=> todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    const getTodos = async ()=>{
+        try {
+            
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
+
+            setTodos(jsonData);
+            
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return (
+        <Fragment>
+           <table className="table table-bordered mt-5 text-center">
+                <thead>
+                    <tr>
+                    <th scope="col">Description</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   {/*  <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr> */}
+                    {todos.map(todo=>(
+                        <tr key={todo.todo_id} >
+                            <td> {todo.description} </td>
+                            <td> Edit </td>
+                            <td>
+                                <button className="btn btn-danger" onClick={()=> deleteTodo(todo.todo_id) } >Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Fragment>
+    )
+}
+
+export default ListTodos
